29b7d8ffd3478397d5ebc1bba5ac376b89e404c5
ryo.k <41412168+ryo1107@users.noreply.github.com>
2018-09-19 22:17:53 +0900

    Update raspi_cpu_gpu_temp.py

--- raspi_cpu_gpu_temp.py
+++ raspi_cpu_gpu_temp.py
@@ -2,17 +2,18 @@
 import os
 import Tkinter
 
-
+#make window
 root = Tkinter.Tk()
 root.title(u"RasPi_temp")
 root.geometry("350x50")
 
-
+#button push command
 def DeleteEntryValue(event):
     cpu_temp,gpu_temp=Temp_get()
     EditBox.delete(0, Tkinter.END)
     EditBox.insert(Tkinter.END,"    CPU_temp="+str(cpu_temp)+"℃  "+"GPU_temp="+gpu_temp+"℃")
 
+#get temp
 def Temp_get():
     
     cpu_file="/opt/vc/bin/vcgencmd measure_temp"
@@ -23,16 +24,14 @@ def Temp_get():
     cpu_temp = cpu_line.split('=')[1].split("'")[0]
     gpu_temp = float(gpu_line)/1000
 
-    
-
     return cpu_temp,str(gpu_temp)
 
-
+#make empty box
 EditBox = Tkinter.Entry(width=35)
 EditBox.insert(Tkinter.END,"")
 EditBox.pack()
 
-
+#button setting
 Button = Tkinter.Button(text=u'raspi_temp', width=25)
 Button.bind("<Button-1>",DeleteEntryValue) 
 
